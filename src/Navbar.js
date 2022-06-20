1f9d7344f11ab51e9a6927b17cccf35e1c38e735
AsleyR <79556058+AsleyR@users.noreply.github.com>
2022-06-19 23:33:12 -0600

    Small change to index.css

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,9 +3,10 @@ import React from "react";
 function Navbar() {
   return (
     <div className="navbar-container">
+      <a href="/"><h2>Task Tracker</h2></a>
       <ul>
         <li>
-          <a href="#">Home</a>
+          <a href="/">Home</a>
         </li>
         <li>
           <a href="#">About</a>
